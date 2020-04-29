e447910b241a69ee99fbf7d106822c9c54833abf
Himel Das <himeldas66@gmail.com>
2020-04-29 13:42:56 +0600

    refactor: 13

--- m3-l3-13-controlled-components/src/App.js
+++ m3-l3-13-controlled-components/src/App.js
@@ -3,21 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
-        </div>
-      </div>
-    );
-  }
+
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query
+        }))
+    }
+
+
+    render() {
+        const { query } = this.state
+
+        return (
+            <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">ReactND - Coding Practice</h1>
+            </header>
+            <div className="container">
+                {/*TODO*/}
+                <input
+                    type="text"
+                    placeholder="Say Something"
+                    value={query}
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                />
+                <p className="echo">Echo:</p>
+                <p>{query}</p>
+            </div>
+            </div>
+        );
+    }
 }
 
 export default App;
